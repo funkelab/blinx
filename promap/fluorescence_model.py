e9d2a332dceea850121668b20f96d88ec22cb772
ahillsley <avhillsley@utexas.edu>
2022-10-07 14:53:06 -0500

    flake8 cleaned up fluorescece_model

--- promap/fluorescence_model.py
+++ promap/fluorescence_model.py
@@ -1,5 +1,3 @@
-import numpy as np
-import math
 import jax
 import jax.numpy as jnp
 from jax import random
@@ -62,61 +60,59 @@ class FluorescenceModel:
         self.sigma_b2 = emission_params.sigma_b**2
         self.label_eff = emission_params.label_eff
 
-
-    
     def sample_x_z_poisson_jax(self, z, seed):
         ''' Samples a Poisson random variable '''
-        lam = self.mu_i * z +self.mu_b
+        lam = self.mu_i * z + self.mu_b
         key = random.PRNGKey(seed)
         key, subkey = random.split(key)
         value = random.poisson(subkey, lam)
-        
+
         return value
-    
-    def sample_x_z_lognorm_jax(self, z, key, shape=(1,1)):
-        #key = random.PRNGKey(seed)
-        #key, subkey = random.split(key)
-        
+
+    def sample_x_z_lognorm_jax(self, z, key, shape=(1, 1)):
+
         mean = jnp.log(self.mu_i * z + self.mu_b)
-        
+
         std_value = random.normal(key, shape)
         value = (std_value * self.sigma_i) + mean
-        
+
         return jnp.exp(value)
-    
+
     def vmap_p_x_given_z_lognorm(self, x, y):
         zs = jnp.arange(0, y+1)
         x = jnp.expand_dims(x, 0)
 
-        result = jax.vmap(self._p_trace_given_z_lognorm, in_axes=(1, None))(x, zs)
+        result = jax.vmap(self._p_trace_given_z_lognorm,
+                          in_axes=(1, None))(x, zs)
 
         return result.T
-    
+
     def vmap_p_x_given_z_poisson(self, x, y):
         zs = jnp.arange(0, y+1)
         x = jnp.expand_dims(x, 0)
 
-        result = jax.vmap(self._p_trace_given_z_poisson, in_axes=(1, None))(x, zs)
+        result = jax.vmap(self._p_trace_given_z_poisson,
+                          in_axes=(1, None))(x, zs)
 
         return result.T
 
-    
     def _p_trace_given_z_poisson(self, trace, z):
         mu = z * self.mu_i + self.mu_b
         prob = jax.scipy.stats.poisson.pmf(trace, mu=mu)
-        
+
         return prob
-    
+
     def _p_trace_given_z_lognorm(self, x, z):
-        
+
         mean = jnp.log(self.mu_i * z + self.mu_b)
         value_1 = jnp.log(x)
         value_2 = value_1 + 1/256
-        
-        prob_1 = jax.scipy.stats.norm.cdf(value_1, loc=mean, scale=self.sigma_i)
-        prob_2 = jax.scipy.stats.norm.cdf(value_2, loc=mean, scale=self.sigma_i)
-        
-        prob = jnp.abs(prob_1 - prob_2)
-        
-        return prob
 
+        prob_1 = jax.scipy.stats.norm.cdf(value_1, loc=mean,
+                                          scale=self.sigma_i)
+        prob_2 = jax.scipy.stats.norm.cdf(value_2, loc=mean,
+                                          scale=self.sigma_i)
+
+        prob = jnp.abs(prob_1 - prob_2)
+
+        return prob
