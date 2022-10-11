5d79b660e44d7b91aeeda2744bb66cf4eebde878
ahillsley <avhillsley@utexas.edu>
2022-10-11 12:33:57 -0500

    renamed consistency to fix typo

--- tests/consistency_test.py
+++ tests/consistency_test.py
@@ -2,7 +2,7 @@ import numpy as np
 import unittest
 from promap.trace_model import TraceModel
 from promap.fluorescence_model import EmissionParams
-from promap.fit_params import FitTrace
+from promap import fit
 
 class TestTraceModel(unittest.TestCase):
     def test_prediction_consistancy(self):
@@ -14,25 +14,19 @@ class TestTraceModel(unittest.TestCase):
         x_trace, states = t_model_t.generate_trace(y_test, seed=seed,
                                                    num_frames=4000)
 
-
-        fit_functions = FitTrace()
-
-        max_likelyhood = -1e8
-        best_y = None
-
         # Calc likelyhood that trace arrose from different y values
         y = 5
 
-        likelyhood, p_on, p_off, mu, sigma = fit_functions.optimize_params(y, x_trace)
+        likelihood, p_on, p_off, mu, sigma = fit.optimize_params(y, x_trace)
 
         print('- '*20)
         print(f'y = {y}')
-        print(f'log likelyhood   = { likelyhood:.2f}')
+        print(f'log likelihood   = { likelihood:.2f}')
         print(f'p_on / p_off     = { p_on:.4f} / {p_off:.4f}')
         print(f'mu / sigma       = {mu:.4f} / {sigma:.4f}')
 
 
-        self.assertAlmostEqual(likelyhood, -16592.64, places=2)
+        self.assertAlmostEqual(likelihood, -16592.64, places=2)
 
 if __name__ == '__main__':
     unittest.main()
