1cd4233a57c2881b671f1946577f53e90170507c
ahillsley <avhillsley@utexas.edu>
2022-10-11 11:51:02 -0500

    added a real consistancy test and removed outdated functions from trace_model

--- promap/trace_model.py
+++ promap/trace_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 from promap.fluorescence_model import FluorescenceModel
-from promap.transition_matrix import TransitionMatrix
+from promap import transition_matrix
 from jax import lax
 import jax.numpy as jnp
 import jax
@@ -9,7 +9,7 @@ from jax import random
 
 class TraceModel:
     '''
-    - Models an intensity trace as a hidden Markov model
+    TODO: replace with a better docstring
 
     Args:
 
@@ -30,39 +30,14 @@ class TraceModel:
             length of trace
     '''
 
-    def __init__(self, emission_params, step_time, num_frames):
+    def __init__(self, emission_params, p_on=None, p_off=None):
 
         # currently working with p_on/off, might need to switch to k_on/off
-        self.p_on = None
-        self.p_off = None
-        self.step_time = step_time
-        self.num_frames = num_frames
+        self.p_on = p_on
+        self.p_off = p_off
         self.fluorescence_model = FluorescenceModel(emission_params)
 
-    def set_params(self, p_on, p_off):
-
-        self.p_on = jnp.float32(p_on)
-        self.p_off = jnp.float32(p_off)
-
-    def fit_params(self, traces, y, method='line_search', **kwargs):
-        # TODO: change so that it calls instance of fit_params
-        '''
-        Fit all the parameters needed for the trace model
-            p_on, p_off
-        '''
-
-        if method == 'line_search':
-            self._line_search_params(traces, y, **kwargs)
-        elif method == 'viterbi':
-            self._viterbi_fit_params(traces, y, **kwargs)
-        return
-
-    def plot_trace():
-        # TODO: make function that plots trace nicely
-
-        return
-
-    def generate_trace(self, y, seed, distribution='lognormal'):
+    def generate_trace(self, y, seed, num_frames, distribution='lognormal'):
         ''' generate a synthetic intensity trace
 
         Args:
@@ -85,8 +60,8 @@ class TraceModel:
                 - array the same shape as x_trace, showing the hiddens state
                     "z" for each frame
                 '''
-        TransMatrix = TransitionMatrix()
-        transition_m = TransMatrix.create_transition_matrix(y, self.p_on,
+        
+        transition_m = transition_matrix.create_transition_matrix(y, self.p_on,
                                                             self.p_off)
 
         # sum(rows) must always be = 1, rounding errors sometimes occur with
@@ -107,9 +82,9 @@ class TraceModel:
         initial_state = jnp.expand_dims(random.categorical(subkey, p_initial),
                                         axis=0)
 
-        scan2 = lambda state, key: self._scan(state, key, transition_m)
+        scan2 = lambda state, key: self._scan_generate(state, key, transition_m)
 
-        subkeys = random.split(key, num=self.num_frames)
+        subkeys = random.split(key, num=num_frames)
         a, states = jax.lax.scan(scan2, init=initial_state, xs=subkeys)
 
         if distribution == 'lognormal':
@@ -123,68 +98,36 @@ class TraceModel:
                                       shape=states.shape)
 
         return x_trace[:, 0], states
+    
+    def get_likelihood(self, probs, transition_m, p_init):
+        ''' 
+        TODO: add a docstring
+        '''
+        initial_values = p_init[:] * probs[:, 0]
+        scale_factor_initial = 1 / jnp.sum(initial_values)
+        initial_values = initial_values * scale_factor_initial
+        p_transition = transition_m
 
-    def _update_state(self, subkey, p_tr):
-        state = random.categorical(subkey, p_tr)
-        return state
+        scan_f_2 = lambda p_accumulate, p_emission: self._scan_likelihood(
+                                                                 p_accumulate,
+                                                                 p_emission,
+                                                                 p_transition)
 
-    def _scan(self, old_state, key, transition_m):
+        final, result = lax.scan(scan_f_2, initial_values, probs.T)
+
+        return -1*(jnp.sum(jnp.log(result)))
+
+    def _scan_generate(self, old_state, key, transition_m):
         p_tr = jnp.log(transition_m[old_state, :])
-        new_state = self._update_state(key, p_tr)
+        new_state = random.categorical(key, p_tr)
 
         return new_state, new_state
-
-    def estimate_y(self, trace, guess, search_width):
-        # TODO: replace with function that
-
-        self._check_parameters()
-
-        log_probs = np.zeros((search_width*2+1))
-        low_bound = 0 if guess - search_width < 0 else guess - search_width
-        for i, y in enumerate(range(low_bound, guess+search_width+1)):
-            log_probs[i] = self.p_trace_given_y(trace, y)
-
-        return log_probs
 
     def _check_parameters(self):
 
         if self.p_on is None:
             raise RuntimeError("Parameters need to be set or fitted first.")
 
-    def _forward_alg(self, x_trace, y, transition_m, p_init):
-
-        forward = np.zeros((y+1, len(x_trace)))
-        scale_factors = np.zeros((len(x_trace)))
-
-        # Initialize
-        for s in range(y+1):
-            forward[s, 0] = p_init[s] * \
-                self.fluorescence_model.p_x_i_given_z_i(x_trace[0], s)
-        scale_factors[0] = 1 / np.sum(forward[:, 0])
-        forward[:, 0] = forward[:, 0] * scale_factors[0]
-
-        # Propagate
-        for t in range(1, len(x_trace)):
-            for state in range(y+1):
-                forward[state, t] = self._alpha(forward, x_trace, transition_m,
-                                                y, state, t)
-
-            scale_factors[t] = 1 / np.sum(forward[:, t])
-            forward[:, t] = forward[:, t] * scale_factors[t]
-
-        # Total likelyhood
-        log_fwrd_prob = -1*(np.sum(np.log(scale_factors)))
-
-        return log_fwrd_prob
-
-    def _alpha(self, forward, x_trace, transition_m, y, s, t):
-        # recursive element of the forward algorithm
-        p = 0
-        for i in range(y+1):
-            p += forward[i, (t-1)] * transition_m[i, s] * \
-                self.fluorescence_model.p_x_i_given_z_i(x_trace[t], s)
-
-        return p
 
     def _scale_viterbi(self, x_trace, y, T, trans_m, p_init):
         "initialize"
@@ -222,21 +165,9 @@ class TraceModel:
             temp[i] = delta[i, t-1] * trans_m[i, s]
         return np.max(temp), np.argmax(temp)
 
-    def _forward_alg_jax(self, probs, transition_m, p_init):
-        initial_values = p_init[:] * probs[:, 0]
-        scale_factor_initial = 1 / jnp.sum(initial_values)
-        initial_values = initial_values * scale_factor_initial
-        p_transition = transition_m
 
-        scan_f_2 = lambda p_accumulate, p_emission: self._scan_f(p_accumulate,
-                                                                 p_emission,
-                                                                 p_transition)
 
-        final, result = lax.scan(scan_f_2, initial_values, probs.T)
-
-        return -1*(jnp.sum(jnp.log(result)))
-
-    def _scan_f(self, p_accumulate, p_emission, p_transition):
+    def _scan_likelihood(self, p_accumulate, p_emission, p_transition):
         '''
         p_accu:
             - accumulated probability from t=0 to t-1
--- tests/consistancy_test.py
+++ tests/consistancy_test.py
@@ -2,39 +2,37 @@ import numpy as np
 import unittest
 from promap.trace_model import TraceModel
 from promap.fluorescence_model import EmissionParams
+from promap.fit_params import FitTrace
 
 class TestTraceModel(unittest.TestCase):
     def test_prediction_consistancy(self):
-        # generate trace
-        sim_trace_len = 2000
-        trace_simulator = TraceModel(EmissionParams(mu_i=100, mu_b=100)
-                                     , 0.1, sim_trace_len)
-        trace_simulator.set_params(0.05, 0.1)
-        trace = trace_simulator.generate_trace(3)
-        
-        # grid search 
-        e_params = EmissionParams(mu_i=100, sigma_i=0.2, mu_b=100)
-        t_model = TraceModel(e_params, 0.1, len(trace))
-        ys = [2, 3, 4]
-
-        points = 5
-        best_ps = np.zeros((len(ys),2))
-        probs = np.zeros((len(ys)))
-
-        for i, y in enumerate(ys):
-            t_model.p_on = None
-            t_model.p_off = None
-            best_ps[i,0], best_ps[i,1] = t_model._line_search_params(trace, y,
-                                                                     points=points,
-                                                                     p_on_max=0.2,
-                                                                     p_off_max=0.2)
-            probs[i] = t_model.p_trace_given_y(trace, y)
-            
-        self.assertAlmostEqual(np.argmax(probs), 1)
-        self.assertAlmostEqual(best_ps[1,0], 0.05, places=3)
-        self.assertAlmostEqual(best_ps[1,1], 0.1, places=3)
+        # generate a test trace
+        y_test = 5
+        seed = 100
+        e_params = EmissionParams(mu_i=50, sigma_i=0.03, mu_b=200, sigma_b=0.15)
+        t_model_t = TraceModel(e_params, p_on=0.05, p_off=0.05)
+        x_trace, states = t_model_t.generate_trace(y_test, seed=seed,
+                                                   num_frames=4000)
 
 
+        fit_functions = FitTrace()
+
+        max_likelyhood = -1e8
+        best_y = None
+
+        # Calc likelyhood that trace arrose from different y values
+        y = 5
+
+        likelyhood, p_on, p_off, mu, sigma = fit_functions.optimize_params(y, x_trace)
+
+        print('- '*20)
+        print(f'y = {y}')
+        print(f'log likelyhood   = { likelyhood:.2f}')
+        print(f'p_on / p_off     = { p_on:.4f} / {p_off:.4f}')
+        print(f'mu / sigma       = {mu:.4f} / {sigma:.4f}')
+
+
+        self.assertAlmostEqual(likelyhood, -16592.64, places=2)
 
 if __name__ == '__main__':
     unittest.main()
