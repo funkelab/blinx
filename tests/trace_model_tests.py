c15fb5619b5f069819cef2c32f64d1c8e95fabc8
ahillsley <avhillsley@utexas.edu>
2023-01-02 13:15:49 -0600

    Outline desired tests for trace_model module

--- tests/trace_model_tests.py
+++ tests/trace_model_tests.py
@@ -2,22 +2,20 @@ import numpy as np
 import unittest
 import pandas as pd
 from promap.trace_model import TraceModel
-from promap.fluorescence_model import EmissionParams
 
 
 class TestTraceModel(unittest.TestCase):
     def test_generate_trace(self):
-        # does the mdoel sucessfully generate a trace of the expercted length
+        # does the model sucessfully generate a trace of the expercted length
         
-        sim_trace_len = 1000
-        seed = 10
-        trace_simulator = TraceModel(EmissionParams(), 0.1, sim_trace_len)
-        trace_simulator.set_params(0.1, 0.1)
-        trace, states = trace_simulator.generate_trace(1, seed)
-
-        self.assertAlmostEqual(len(trace), sim_trace_len)
-
         return
+    
+    def test_get_likelihood(self):
+        # does the model return the expected likelihood value for known params
+        
+        return
+    
+    
 
         
 if __name__ == '__main__':
