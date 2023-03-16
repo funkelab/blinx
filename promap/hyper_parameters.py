3624f607f2222d3310fc5796dcfe6699ab5ce444
ahillsley <avhillsley@utexas.edu>
2023-03-16 17:09:14 -0500

    Add new argument for a distribution comaprrison post processing threshold

--- promap/hyper_parameters.py
+++ promap/hyper_parameters.py
@@ -26,10 +26,12 @@ class HyperParameters:
             num_guesses=5,
             epoch_length=1000,
             is_done_limit=1e-5,
-            mu_gradient_step_size=1e-3):
+            mu_gradient_step_size=1e-3,
+            distribution_threshold=2e-1):
 
         self.gradient_step_size = gradient_step_size
         self.num_guesses = num_guesses
         self.epoch_length = epoch_length
         self.is_done_limit = is_done_limit
         self.mu_gradient_step_size = mu_gradient_step_size
+        self.distribution_threshold=distribution_threshold
