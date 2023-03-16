ada8a83d2977392ada40b5ce16fa4d7efcc1a861
ahillsley <avhillsley@utexas.edu>
2023-03-16 17:13:48 -0500

    Flake8

--- promap/post_process.py
+++ promap/post_process.py
@@ -8,10 +8,7 @@ from promap.constants import (
 from promap.fluorescence_model import FluorescenceModel
 from promap.trace_model import TraceModel
 from promap import transition_matrix
-from promap.hyper_parameters import HyperParameters
-import jax
 import jax.numpy as jnp
-
 
 
 def post_process(
@@ -21,45 +18,45 @@ def post_process(
         y_low,
         hyper_parameters):
     '''
-    Big wrapper function that combines all post processing steps and returns 
+    Big wrapper function that combines all post processing steps and returns
     the best y guess for each trace
-    
+
     Inputs: likelihoods, parameters, traces, ys_tested
-    
+
     '''
     # find max likelihood and use it as value to replace bad values with
     sub_value = jnp.max(likelihoods[jnp.isfinite(likelihoods)])
-    
-    
+
     # remove nan likelihoods
     proc_likelihoods = likelihoods.at[jnp.isnan(likelihoods)].set(sub_value)
-    
+
     # comapre differences in distributions
     dist_diffs = compare_dists(traces, parameters, y_low)
     likes_to_remove = dist_diffs > hyper_parameters.distribution_threshold
-    
+
     proc_likelihoods = proc_likelihoods.at[likes_to_remove].set(sub_value)
-    
+
     # Add other post_processing here
-    
+
     # find new most likely y values
     most_likely_ys = jnp.argmin(proc_likelihoods, axis=0) + y_low
 
     return most_likely_ys, proc_likelihoods
 
+
 def compare_dists(
         traces,
         parameters,
         y_low):
-    
+
     viterbi_traces, viterbi_dist = viterbi(traces, y_low, parameters)
     steady_state_dist = steady_state(parameters, y_low)
-    
+
     diffs = np.abs(viterbi_dist - steady_state_dist)
-    
+
     dist_differences = np.sum(diffs, axis=2)
-    
-    return  dist_differences
+
+    return dist_differences
 
 
 def viterbi(traces, params, y_low):
@@ -67,7 +64,7 @@ def viterbi(traces, params, y_low):
     inputs: traces, paramters, ys_tested
     returns: viterbi traces in same shape of traces
     '''
-    
+
     viterbi_traces = np.zeros((
         params.shape[0],
         traces.shape[0],
@@ -75,28 +72,28 @@ def viterbi(traces, params, y_low):
     vit_dists = np.zeros((
         params.shape[0],
         params.shape[1],
-        params.shape[0]+1))
+        params.shape[0] + 1))
     for i, y in enumerate(range(y_low, y_low+params.shape[0])):
         for t in range(traces.shape[0]):
             f_model = FluorescenceModel(
-                mu_i=params[i, t ,PARAM_MU],
+                mu_i=params[i, t, PARAM_MU],
                 mu_b=params[i, t, PARAM_MU_BG],
                 sigma=params[i, t, PARAM_SIGMA])
             t_model = TraceModel(
                 f_model,
-                p_on=params[i, t ,PARAM_P_ON],
+                p_on=params[i, t, PARAM_P_ON],
                 p_off=params[i, t, PARAM_P_OFF])
-            
-            vit_trace = t_model.viterbi_alg(y, traces[t,:])
+
+            vit_trace = t_model.viterbi_alg(y, traces[t, :])
             vit_dists[i, t, :], _ = np.histogram(
                 vit_trace,
                 density=True,
-                bins=range(params.shape[0]+2))
-            
+                bins=range(params.shape[0] + 2))
+
             viterbi_traces[i, t, :] = vit_trace
 
-    
     return viterbi_traces, vit_dists
+
 
 def steady_state(parameters, y_low):
     ss_dists = np.zeros((
@@ -109,8 +106,7 @@ def steady_state(parameters, y_low):
                 y=y,
                 p_on=parameters[i, t, PARAM_P_ON],
                 p_off=parameters[i, t, PARAM_P_OFF],)
-                
+
             ss_dists[i, t, :y+1] = transition_matrix.p_initial(y, trans_matrix)
 
     return ss_dists
-
