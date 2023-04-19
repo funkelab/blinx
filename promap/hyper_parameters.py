bb279d7bf92a3075e333b8a47d35f0935911b992
Jan Funke <funkej@janelia.hhmi.org>
2023-04-19 14:02:16 -0400

    Add number of bins and max x value to hyper parameters

--- promap/hyper_parameters.py
+++ promap/hyper_parameters.py
@@ -18,16 +18,6 @@ class HyperParameters:
             into smaller stretches (epochs). Convergence is tested for at the
             end of each epoch.
 
-        discretize_x (``bool``):
-
-            If to discretize the measurements. Discretization will consume less
-            memory, but is not as accurate as using the original measurements.
-
-        discrete_x_dtype (``jnp.dtype``):
-
-            Datatype of the discretized x values (``jax.numpy.uint8`` or
-            ``jax.numpy.uint16``).
-
         max_x_value (``int``):
 
             The maximal x value to consider for discretizing x.
@@ -43,9 +33,8 @@ class HyperParameters:
             is_done_limit=1e-5,
             mu_gradient_step_size=1e-3,
             distribution_threshold=1e-1,
-            discretize_x=False,
-            discrete_x_dtype='uint16',
-            max_x_value=None):
+            max_x_value=None,
+            num_x_bins=1024):
 
         self.y_low = y_low
         self.gradient_step_size = gradient_step_size
@@ -54,6 +43,5 @@ class HyperParameters:
         self.is_done_limit = is_done_limit
         self.mu_gradient_step_size = mu_gradient_step_size
         self.distribution_threshold=distribution_threshold
-        self.discretize_x = discretize_x
-        self.discrete_x_dtype = discrete_x_dtype
         self.max_x_value = max_x_value
+        self.num_x_bins = num_x_bins
