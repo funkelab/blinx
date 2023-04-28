8a63d31ef3029e5a599d90c6ae7e9c335dae6404
Jan Funke <funkej@janelia.hhmi.org>
2023-04-28 16:55:22 -0400

    Add (simple) trace model test

---
+++ tests/test_trace_model.py
@@ -0,0 +1,24 @@
+from promap import HyperParameters
+from promap.trace_model import get_trace_log_likelihood
+import jax
+import numpy as np
+import pytest
+
+
+def test_gradients(trace_with_groundtruth):
+
+    trace = trace_with_groundtruth["trace"]
+    parameters = trace_with_groundtruth["parameters"]
+    y = trace_with_groundtruth["y"]
+
+    hyper_parameters = HyperParameters()
+    hyper_parameters.max_x = 2 * y * parameters.mu
+
+    value_and_gradients = jax.value_and_grad(
+        lambda p: get_trace_log_likelihood(trace, y, p, hyper_parameters)
+    )
+
+    log_likelihood, gradients = value_and_gradients(parameters)
+    gradients = np.asarray(gradients)
+
+    assert not np.any(np.isnan(gradients))
