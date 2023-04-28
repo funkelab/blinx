91e663b5cfba0672db6520a32f71268184eb00b9
Jan Funke <funkej@janelia.hhmi.org>
2023-04-28 16:54:13 -0400

    Add first tests for estimate.py

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+from promap.parameters import Parameters
+from promap.trace_model import generate_trace
+import jax.numpy as jnp
+import pytest
+
+
+@pytest.fixture(scope="module")
+def trace_with_groundtruth():
+    """Create a random trace for other tests to work on."""
+
+    y = 5
+    parameters = Parameters(mu=100.0, mu_bg=10.0, sigma=0.001, p_on=0.1, p_off=0.3)
+    num_frames = 1000
+
+    trace, zs = generate_trace(y, parameters, num_frames)
+
+    return {
+        "trace": trace,
+        "parameters": parameters,
+        "y": y,
+        "zs": zs,
+    }
---
+++ tests/test_estimate.py
@@ -0,0 +1,73 @@
+from promap import HyperParameters, ParameterRanges
+from promap.estimate import get_initial_parameter_guesses, estimate_parameters
+import jax.numpy as jnp
+
+
+def test_parameter_guesses(trace_with_groundtruth):
+
+    trace = trace_with_groundtruth["trace"]
+    traces = jnp.expand_dims(trace, axis=0)
+    parameters = trace_with_groundtruth["parameters"]
+    y = trace_with_groundtruth["y"]
+
+    hyper_parameters = HyperParameters()
+    hyper_parameters.max_x = 2 * y * parameters.mu
+    hyper_parameters.num_guesses = 1
+
+    # create parameter ranges that contain the true values
+    parameter_ranges = ParameterRanges(
+        mu_range=(parameters.mu - 100.0, parameters.mu + 100.0),
+        mu_bg_range=(parameters.mu_bg - 5.0, parameters.mu_bg + 5.0),
+        sigma_range=(parameters.sigma, parameters.sigma),
+        p_on_range=(parameters.p_on, parameters.p_on),
+        p_off_range=(parameters.p_off, parameters.p_off),
+        mu_step=3,
+        mu_bg_step=3,
+        sigma_step=1,
+        p_on_step=1,
+        p_off_step=1
+    )
+
+    parameter_guesses = get_initial_parameter_guesses(
+        traces,
+        y,
+        parameter_ranges,
+        hyper_parameters)
+
+    # should return only one guess
+    assert len(parameter_guesses.mu) == 1
+
+    # ...and that should be the true one
+    assert parameter_guesses.mu == parameters.mu
+
+
+def test_inference(trace_with_groundtruth):
+
+    trace = trace_with_groundtruth["trace"]
+    traces = jnp.expand_dims(trace, axis=0)
+    parameters = trace_with_groundtruth["parameters"]
+    y = trace_with_groundtruth["y"]
+
+    hyper_parameters = HyperParameters()
+    hyper_parameters.max_x = 2 * y * parameters.mu
+    hyper_parameters.num_guesses = 1
+
+    # create parameter ranges that contain the true values
+    parameter_ranges = ParameterRanges(
+        mu_range=(parameters.mu - 100.0, parameters.mu + 100.0),
+        mu_bg_range=(parameters.mu_bg - 5.0, parameters.mu_bg + 5.0),
+        sigma_range=(parameters.sigma, parameters.sigma),
+        p_on_range=(parameters.p_on, parameters.p_on),
+        p_off_range=(parameters.p_off, parameters.p_off),
+        mu_step=3,
+        mu_bg_step=3,
+        sigma_step=1,
+        p_on_step=1,
+        p_off_step=1
+    )
+
+    parameters, likelihood = estimate_parameters(
+        traces,
+        y,
+        parameter_ranges,
+        hyper_parameters)
