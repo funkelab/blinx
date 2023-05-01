1b5d78b2c2f3a8ac8ddda95a040dcec3cf47bb49
Jan Funke <funkej@janelia.hhmi.org>
2023-05-01 14:17:53 -0400

    Delete obsolete code

--- promap/protein_model/__init__.py
+++
@@ -1,6 +0,0 @@
-from .amino_acids import AminoAcid
-from .fluorescence_model import FluorescenceModel
-from .protein_db import ProteinDb
-from .sequence import count_amino_acids
-
-__all__ = ["AminoAcid", "FluorescenceModel", "ProteinDb", "count_amino_acids"]
