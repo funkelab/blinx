394549e2359a4b1c9253db186da84fa62cbfe0b9
ahillsley <avhillsley@utexas.edu>
2023-05-03 13:43:05 -0400

    Add another trace with higher sigma for testing inference

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,20 @@ def trace_with_groundtruth():
         "y": y,
         "zs": zs,
     }
+
+@pytest.fixture(scope="module")
+def trace_with_groundtruth_noisy():
+    """Create a random trace for other tests to work on."""
+
+    y = 5
+    parameters = Parameters(mu=100.0, mu_bg=10.0, sigma=0.03, p_on=0.05, p_off=0.05)
+    num_frames = 1000
+
+    trace, zs = generate_trace(y, parameters, num_frames)
+
+    return {
+        "trace": trace,
+        "parameters": parameters,
+        "y": y,
+        "zs": zs,
+    }
