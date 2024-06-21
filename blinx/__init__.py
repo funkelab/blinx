880d48e01c0d97c48263f63b4923088777be4c90
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2024-06-21 11:11:57 -0400

    export Parameters from the main blinx module

--- blinx/__init__.py
+++ blinx/__init__.py
@@ -1,8 +1,10 @@
+from .parameters import Parameters
 from .parameter_ranges import ParameterRanges  # noqa
 from .hyper_parameters import HyperParameters, create_step_sizes
 from .estimate import estimate_y, estimate_parameters
 
 __all__ = [
+    "Parameters",
     "ParameterRanges",
     "HyperParameters",
     "create_step_sizes",
